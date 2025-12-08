a129221cda51bf1b60736103a9feed9065beb2dd
FernandaDwi999 <fernandadchy@gmail.com>
2025-12-08 14:51:38 +0700

    create gallery detail pop-up

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
+// navbar
 window.addEventListener("scroll", function () {
-  const nav = document.getElementById("navbar");
+  const nav = document.getElementById("nav-bg");
   if (window.scrollY > 10) {
     nav.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
   }
+});
+
+// efek typing di hero section
+const text = "Explore The Beauty of Space";
+let index = 0;
+
+function typeEffect() {
+  if (index < text.length) {
+    document.querySelector(".type-text").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, 50);
+  }
+}
+
+typeEffect();
+
+// Popup detail modal
+const planetData = {
+  Merkurius: "Planet paling dekat dengan Matahari.",
+  Venus: "Planet terpanas karena efek rumah kaca.",
+  Bumi: "Satu-satunya planet yang mendukung kehidupan.",
+  Mars: "Disebut planet merah.",
+  Jupiter: "Planet terbesar di tata surya.",
+  Saturnus: "Dikenal dengan cincinnya.",
+  Uranus: "Berputar menyamping.",
+  Neptunus: "Planet terjauh & sangat dingin.",
+};
+
+document.querySelectorAll(".planet-box").forEach((box) => {
+  box.addEventListener("click", () => {
+    const title = box.querySelector("h5").innerText;
+    const img = box.querySelector("img").src;
+
+    document.getElementById("planetTitle").innerText = title;
+    document.getElementById("planetImage").src = img;
+    document.getElementById("planetDesc").innerText = planetData[title];
+
+    const modal = new bootstrap.Modal(document.getElementById("planetModal"));
+    modal.show();
+  });
 });
